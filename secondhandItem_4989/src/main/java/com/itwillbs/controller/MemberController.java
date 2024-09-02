21dcf1c0c4f62b852f6173bfa6ed142035f9d5a6
ITWILL <ITWILL@DESKTOP-5J6GM60>
2024-09-02 11:22:48 +0900

    login페이지 화면 폼과 membercontroller 추가

--- secondhandItem_4989/src/main/java/com/itwillbs/controller/MemberController.java
+++ secondhandItem_4989/src/main/java/com/itwillbs/controller/MemberController.java
@@ -1,5 +1,18 @@
 package com.itwillbs.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@GetMapping("/login")
+	public String intro() {
+		return "/member/login";
+	}
+	
+	
+	
 }
