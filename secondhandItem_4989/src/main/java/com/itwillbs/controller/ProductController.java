93d82173bb655b33cf1eea3ab95047738c7c0c93
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-09-04 15:59:24 +0900

    register 백엔드 작업중

--- secondhandItem_4989/src/main/java/com/itwillbs/controller/ProductController.java
+++ secondhandItem_4989/src/main/java/com/itwillbs/controller/ProductController.java
@@ -1,23 +1,34 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import com.itwillbs.domain.ProductDTO;
+import com.itwillbs.service.ProductService;
+
 @Controller
 @RequestMapping("/product/*")
 public class ProductController {
+	@Inject
+	private ProductService productService;
+	
 	@GetMapping("/register")
-	public String main1() {
+	public String main() {
 		return "/product/register";
 	}
-	
-	
-	@GetMapping("/all")
-	public String main() {
-		return "/product/all";
+
+	@PostMapping("/registerPro")
+	public String registerPro(ProductDTO productDTO) {
+		productService.insertProduct(productDTO);
+		return "redirect:/prouct/all";
+		
 	}
+
 
 }//ProductController()
 
--- secondhandItem_4989/src/main/java/com/itwillbs/dao/ProductDAO.java
+++ secondhandItem_4989/src/main/java/com/itwillbs/dao/ProductDAO.java
@@ -1,8 +1,29 @@
 package com.itwillbs.dao;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import com.itwillbs.domain.ProductDTO;
 
 @Repository
 public class ProductDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String namespace="com.itwillbs.mapper.ProductMapper";
+
+	public int getMaxNum() {
+		System.out.println("BoardDAO getMaxNum()");
+		return sqlSession.selectOne(namespace + ".getMaxNum");
+	}
+
+	public void insertProduct(ProductDTO productDTO) {
+		sqlSession.insert(namespace + ".insertProduct", productDTO);
+		
+	}
+
 
 }
