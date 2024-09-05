85940fc7852bdd4f8d6d8f9f764eec620fcd69ac
ITWILL <ITWILL@DESKTOP-5J6GM60>
2024-09-05 16:23:29 +0900

    admin으로 로그인 안했을경우 admin 메인페이지에 접근 차단

--- secondhandItem_4989/src/main/java/com/itwillbs/controller/AdminController.java
+++ secondhandItem_4989/src/main/java/com/itwillbs/controller/AdminController.java
@@ -1,17 +1,36 @@
 package com.itwillbs.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
+	//admin이 아닐경우 admin페이지에 접근차단
 	@GetMapping("/")
-	public String main() {
-		return "/admin/sample";
-	}
+    public String main(HttpSession session, RedirectAttributes redirectAttributes) {
+        String memberId = (String) session.getAttribute("member_id");
+
+        // member_id가 admin이 아닌 경우 메인 페이지로 리다이렉트
+        if (memberId == null || !memberId.equals("admin")) {
+            redirectAttributes.addFlashAttribute("msg", "접근 권한이 없습니다.");
+            return "redirect:/";  // 메인 페이지로 리다이렉트
+        }
+
+        // member_id가 admin인 경우 admin 페이지로 이동
+        return "/admin/sample";
+    }
+	
+	
+//	@GetMapping("/")
+//	public String main() {
+//		return "/admin/sample";
+//	}
 	
 //	@GetMapping("/member")
 //	public String member() {
