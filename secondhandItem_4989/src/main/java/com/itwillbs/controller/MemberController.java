f2381dd2f583ed3481f755f21b6e0fb262641077
ITWILL <ITWILL@DESKTOP-5J6GM60>
2024-09-10 18:56:24 +0900

    Merge branch 'Dev' of https://github.com/Fire-Lights-1s/itwill_project_1_4989.git into Dev

--- secondhandItem_4989/src/main/java/com/itwillbs/controller/MemberController.java
+++ secondhandItem_4989/src/main/java/com/itwillbs/controller/MemberController.java
@@ -1,13 +1,16 @@
 package com.itwillbs.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwillbs.domain.MemberDTO;
 import com.itwillbs.service.MemberService;
@@ -19,10 +22,42 @@ public class MemberController {
 	@Inject
 	private MemberService memberService;
 	
+//	@GetMapping("/login")
+//	public String login() {
+//		return "/member/login";
+//	}
+	
 	@GetMapping("/login")
-	public String login() {
-		return "/member/login";
-	}
+    public String loginPage(HttpServletRequest request, HttpSession session, MemberDTO memberDTO) {
+        // 쿠키에서 loginSessionId를 확인하여 자동 로그인 처리
+        Cookie[] cookies = request.getCookies();
+        System.out.println(cookies);
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("loginSessionId".equals(cookie.getName())) {
+                	
+                	String id = cookie.getValue();
+                	
+                	memberDTO.setMember_id(id);
+                	
+                	MemberDTO memberDTO2 = memberService.userCheck1(memberDTO);
+            		System.out.println(memberDTO2);
+            		if(memberDTO2 != null) {
+            			
+            			session.setAttribute("member_id", memberDTO2.getMember_id());
+            			session.setAttribute("nickname", memberDTO2.getNickname());
+            			// 쿠키에 저장된 세션 ID가 현재 세션과 일치할 경우 자동 로그인
+                        return "redirect:/";
+                        
+            		}
+                        
+                    
+                }
+            }
+        }
+        // 로그인 페이지로 이동
+        return "/member/login";
+    }
 	
 	@GetMapping("/join")
 	public String join() {
@@ -36,23 +71,58 @@ public class MemberController {
 	
 	
 	
+//	//로그인 처리기능
+//	@PostMapping("/loginPro")
+//	public String loginPro(MemberDTO memberDTO,HttpSession session) {
+//		
+//		MemberDTO memberDTO2 = memberService.userCheck(memberDTO);
+//		System.out.println(memberDTO2);
+//		if(memberDTO2 != null) {
+//			
+//			session.setAttribute("member_id", memberDTO2.getMember_id());
+//			session.setAttribute("nickname", memberDTO2.getNickname());
+//			
+//			return "redirect:/";
+//		}else {
+//			
+//			return "redirect:/member/login";
+//		}
+//	}
+	
 	//로그인 처리기능
-	@PostMapping("/loginPro")
-	public String loginPro(MemberDTO memberDTO,HttpSession session) {
-		
-		MemberDTO memberDTO2 = memberService.userCheck(memberDTO);
-		System.out.println(memberDTO2);
-		if(memberDTO2 != null) {
+		@PostMapping("/loginPro")
+		public String loginPro(MemberDTO memberDTO,
+				HttpSession session,
+				@RequestParam(value = "autoLogin", required = false) String autoLogin,
+				HttpServletResponse response) {
 			
-			session.setAttribute("member_id", memberDTO2.getMember_id());
-			session.setAttribute("nickname", memberDTO2.getNickname());
-			
-			return "redirect:/";
-		}else {
-			
-			return "redirect:/member/login";
+			MemberDTO memberDTO2 = memberService.userCheck(memberDTO);
+			System.out.println(memberDTO2);
+			if(memberDTO2 != null) {
+				
+				session.setAttribute("member_id", memberDTO2.getMember_id());
+				session.setAttribute("nickname", memberDTO2.getNickname());
+				
+				String id = (String) session.getAttribute("member_id");
+				
+				// 자동 로그인이 체크된 경우
+	            if (autoLogin != null) {
+	                // 세션 아이디를 쿠키로 저장 (유효 기간 30일 설정)
+	                Cookie loginCookie = new Cookie("loginSessionId", id);
+	                loginCookie.setMaxAge(30 * 24 * 60 * 60); // 쿠키 유효 기간 30일 설정
+	                loginCookie.setPath("/"); // 웹사이트 전체에서 쿠키가 유효
+	                response.addCookie(loginCookie);
+	                System.out.println(loginCookie);
+	            }
+	            
+				
+				return "redirect:/";
+			}else {
+				
+				return "redirect:/member/login";
+			}
 		}
-	}
+
 	
 	@GetMapping("/welcome")
 	public String welcome() {
@@ -82,12 +152,27 @@ public class MemberController {
 	}
 	
 	//로그아웃 처리기능(세션값 제거)
+//	@GetMapping("/logout")
+//	public String logout(HttpSession session) {
+//		System.out.println("MemberController logout");
+//		session.invalidate();
+//		return "redirect:/";
+//	}
+	
 	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-		System.out.println("MemberController logout");
-		session.invalidate();
-		return "redirect:/";
-	}
+    public String logout(HttpSession session, HttpServletResponse response) {
+        // 세션 무효화
+        session.invalidate();
+
+        // 자동 로그인 쿠키 삭제
+        Cookie loginCookie = new Cookie("loginSessionId", null);
+        loginCookie.setMaxAge(0); // 쿠키를 삭제하는 방식으로 만료시킴
+        loginCookie.setPath("/");
+        response.addCookie(loginCookie);
+
+        
+        return "redirect:/";
+    }
 	
 	@GetMapping("/naver")
 	public String naver() {
