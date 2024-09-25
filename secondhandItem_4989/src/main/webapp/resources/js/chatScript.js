08587b1187c2646c7b7f78ebaf4f9b62a52c5a94
subsub <bs083681@gmail.com>
2024-09-25 10:59:29 +0900

    채팅방 구매 확정시 버그 수정

--- secondhandItem_4989/src/main/webapp/resources/js/chatScript.js
+++ secondhandItem_4989/src/main/webapp/resources/js/chatScript.js
@@ -324,7 +324,7 @@ function promiseTrade(){
 	    	let data = {
 	    	"product_id":productGlobal.product_id,
 	    	"seller_id":productGlobal.seller_id,
-	    	"trade_method":productGlobal.pay_method,
+	    	"pay_method":productGlobal.pay_method,
 	    	"trade_status":tradeState,
 	    	"buyer_id": chatRoomGlobal.buyer_id,
 	    	}
